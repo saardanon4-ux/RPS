cbfbef9d61e3cde44342009f87207abaf3f337ff
Saar Danon <saardanon4@gmail.com>
2026-02-06 11:45:20 +0200

    update 6/2

--- client/src/components/WelcomeScreen.jsx
+++ client/src/components/WelcomeScreen.jsx
@@ -62,8 +62,8 @@ export default function WelcomeScreen() {
 
       <motion.div
         className="w-full max-w-md"
-        initial={{ opacity: 0, y: 20 }}
-        animate={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
         transition={{ duration: 0.5 }}
       >
         <div
